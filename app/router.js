7a075a150e48636dadea89ff71c0accc15cb442a
Dean Longmire <deanlongmire100@gmail.com>
2024-05-30 17:22:04 -0400

    ember init

---
+++ app/router.js
@@ -0,0 +1,9 @@
+import EmberRouter from '@ember/routing/router';
+import config from 'b-and-g-darts/config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
+
+Router.map(function () {});
